92d219e3c1c706ed61f48c18f6c00ce0d12a8ac9
EthanJacob02 <ethanj02@email.unc.edu>
2022-04-18 15:07:21 -0400

    debug endpoints added.

--- server.js
+++ server.js
@@ -47,8 +47,24 @@ app.use( (req, res, next) => {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)') 
+    stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, logdata.status, logdata.referer, logdata.useragent) 
     next();
 })
+
+// Debug endpoints
+if (args.debug) {
+    app.get('/app/log/access', (req, res) => {
+        const stmt = db.prepare('SELECT * from accesslog').all()
+        res.status(200).json(stmt)
+    });
+
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.')
+    });
+}
+
+
 
 app.get('/app', (req, res) => {
     // Respond with status 200
